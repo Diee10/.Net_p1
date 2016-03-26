1b9f9150ddc1841b61b2740f014592c6a99d12f6
Dario <levy.dario@gmail.com>
2016-03-26 12:05:23 -0300

    Todooooo

---
+++ DataAccessLayer/DALEmployeesMongo.cs
@@ -0,0 +1,79 @@
+﻿using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Bson.Serialization;
+
+namespace DataAccessLayer
+{
+    public class DALEmployeesMongo : IDALEmployees
+    {
+        MongoClient client;
+        IMongoDatabase database;
+        IMongoCollection<Employee> collection;
+
+        public DALEmployeesMongo()
+        {
+            BsonClassMap.RegisterClassMap<Employee>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+                cm.SetIsRootClass(true);
+            });
+
+            BsonClassMap.RegisterClassMap<PartTimeEmployee>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+            });
+
+            BsonClassMap.RegisterClassMap<FullTimeEmployee>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+            });
+            client      = new MongoClient("mongodb://localhost");
+            database    = client.GetDatabase("local");
+            collection  = database.GetCollection<Employee>("employees");
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            this.collection.InsertOneAsync(emp);
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var filter = Builders<Employee>.Filter.Eq("id", id);
+            this.collection.DeleteOneAsync(filter);
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            var filter = Builders<Employee>.Filter.Eq("id", emp.Id);
+            var update = Builders<Employee>.Update.Set("Name", emp.Name).Set("StartDate", emp.StartDate);
+            var result = collection.UpdateOneAsync(filter, update);
+
+            //return result;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> result = new List<Employee>();
+            result.AddRange(this.collection.FindAsync(new BsonDocument()).Result.ToList());
+            return result;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            var filter = Builders<Employee>.Filter.Eq("id", id);
+            Employee employee = (Employee)this.collection.Find(filter);
+            return employee;
+        }
+    }
+}
---
+++ PresentationLayerWinform/EmployeeList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogicLayer;
+using DataAccessLayer;
+using Shared.Entities;
+
+namespace PresentationLayerWinform
+{
+    public partial class EmployeeList : Form
+    {
+        public EmployeeList()
+        {
+            InitializeComponent();
+        }
+
+        private void EmployeeList_Load(object sender, EventArgs e)
+        {
+   
+
+            
+            
+            IBLEmployees _ble = new BLEmployees(new DALEmployeesMongo());
+            List<Employee> employeeList = _ble.GetAllEmployees();
+
+            listBox1.DataSource = employeeList;
+            listBox1.DisplayMember = "Name";
+            listBox1.ValueMember = "Id";
+        }
+
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(272, 238);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.Load += new System.EventHandler(this.EmployeeList_Load);
+            // 
+            // EmployeeList
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listBox1);
+            this.Name = "EmployeeList";
+            this.Load += new System.EventHandler(this.EmployeeList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // throw new Exception();
+        }
+    }
+}

3db6320d5dcc62ca1b0da194cdd9065209004812
Andres Botta <a.botta@case-inc.com>
2016-04-06 22:23:26 -0300

    arreglado todo el quilombo

--- ServiceLayer/Program.cs
+++ ServiceLayer/Program.cs
@@ -1,4 +1,4 @@
-﻿using BusinessLogicLayer;
+﻿:using BusinessLogicLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +6,8 @@ using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Practices.Unity;
-using Microsoft.Practices.Unity.Configuration;
+//using Microsoft.Practices.Unity;
+//using Microsoft.Practices.Unity.Configuration;
 
 namespace Microsoft.ServiceModel.Samples
 {
@@ -23,19 +23,43 @@ namespace Microsoft.ServiceModel.Samples
 
         private static void SetupDependencies()
         {
-            IUnityContainer container = new UnityContainer();
-            container.LoadConfiguration();
+            //IUnityContainer container = new UnityContainer();
+            //container.LoadConfiguration();
 
-            IBLEmployees blHandler = container.Resolve<IBLEmployees>();
+            //IBLEmployees blHandler = container.Resolve<IBLEmployees>();
 
-            blHandler.GetAllEmployees();
+            //blHandler.GetAllEmployees();
 
-            //blHandler = new BLEmployees(new DataAccessLayer.DALEmployeesEF());
+            blHandler = new BLEmployees(new DataAccessLayer.DALEmployeesEF());
         }
 
         private static void SetupService()
         {
-            
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.Name = "binding1";
+            binding.HostNameComparisonMode = HostNameComparisonMode.StrongWildcard;
+            binding.Security.Mode = BasicHttpSecurityMode.None;
+
+            Uri baseAddress = new Uri("http://localhost:8834/tsi1/ServiceModelSamples/Service/");
+            Uri address = new Uri("http://localhost:8834/tsi1/ServiceModelSamples/Service/ServiceEmployees");
+
+            // Create a ServiceHost for the CalculatorService type and provide the base address.
+            ServiceHost serviceHost = new ServiceHost(typeof(ServiceEmployees), baseAddress);
+
+            serviceHost.AddServiceEndpoint(typeof(IServiceEmployees), binding, address);
+
+            // Open the ServiceHostBase to create listeners and start listening for messages.
+            serviceHost.Open();
+
+            // The service can now be accessed.
+            Console.WriteLine("The service is ready.");
+            Console.WriteLine("Press <ENTER> to terminate service.");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            // Close the ServiceHostBase to shutdown the service.
+            serviceHost.Close();
         }
     }
 }
+
